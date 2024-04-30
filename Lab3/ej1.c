ea8651f2108651352b932cea65badb3ede25aa21
Fabian <fabian@Ubuntu.myguest.virtualbox.org>
2024-04-30 00:05:31 -0600

    resolucion del primer ejercicio del lab, simple implementacion de un ciclo for para este

---
+++ Lab3/ej1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main () {   
+
+    int i;      
+    printf("1 \n");
+
+    for (i = 0; i < 100; i += 3) {
+
+    printf("%d ", i);
+
+    
+        }
+    printf("\n");
+
+    return 0;
+
+}
